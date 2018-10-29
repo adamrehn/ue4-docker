cb4e53a490866093b268d0f965e94eeaff964a21
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-10-29 13:10:55 +1000

    Fix reference to removed variable in build script

--- build.py
+++ build.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 		
 		# Build the minimal UE4 CI image, unless requested otherwise by the user
 		buildUe4Minimal = config.noMinimal == False
-		if buildUe4Package == True:
+		if buildUe4Minimal == True:
 			builder.build('ue4-minimal', mainTag, config.platformArgs + ue4BuildArgs, config.rebuild, config.dryRun)
 		else:
 			logger.info('User specified `--no-minimal`, skipping ue4-minimal image build.')
