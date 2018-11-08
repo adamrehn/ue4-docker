e00d9acb40f88d7a241dc58784ad2e671aa8f39d
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-11-08 12:41:24 +1000

    4.21 compat: flexible detection of bundled clang for linker fixup

---
+++ dockerfiles/ue4-engine/linux/linker-fixup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import glob, os, shutil, sys
+
+# Retrieve the Linux SDK root directory and system ld location from our command-line arguments
+sdkRoot = sys.argv[1]
+systemLd = sys.argv[2]
+
+# Locate the bundled version(s) of ld and replace them with symlinks to the system ld
+for bundled in glob.glob(os.path.join(sdkRoot, '*', 'x86_64-unknown-linux-gnu', 'bin', 'x86_64-unknown-linux-gnu-ld')):
+	os.unlink(bundled)
+	os.symlink(systemLd, bundled)
+	print('{} => {}'.format(bundled, systemLd), file=sys.stderr)
