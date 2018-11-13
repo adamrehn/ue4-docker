7640baed42bcc7a6eda38186766c0fb2d2901d00
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-11-13 15:17:32 +1000

    Refactor Windows version detection functionality

--- ue4docker/infrastructure/WindowsUtils.py
+++ ue4docker/infrastructure/WindowsUtils.py
@@ -9,22 +9,11 @@ semver = PackageUtils.importFile('semver', os.path.join(PackageUtils.getPackageL
 
 class WindowsUtils(object):
 	
-	# Sentinel value indicating a Windows Insider preview build
-	_insiderSentinel = 'Windows Insider Preview'
+	# The latest Windows build version we recognise as a non-Insider build
+	_latestReleaseBuild = 17134
 	
 	# The list of Windows Server Core base image tags that we support, in ascending version number order
 	_validTags = ['ltsc2016', '1709', '1803']
-	
-	@staticmethod
-	def isSupportedWindowsVersion():
-		'''
-		Verifies that the Windows host system is Windows 10 / Windows Server 2016 version 1607 or newer
-		
-		(1607 is the first build to support Windows containers, as per:
-		<https://docs.microsoft.com/en-us/virtualization/windowscontainers/deploy-containers/version-compatibility>)
-		'''
-		version = semver.parse(platform.win32_ver()[1])
-		return version['major'] == 10 and version['patch'] >= 14393
 	
 	@staticmethod
 	def formatSystemName(release):
@@ -34,14 +23,11 @@ class WindowsUtils(object):
 		return 'Windows {} version {}'.format('Server' if WindowsUtils.isWindowsServer() else '10', release)
 	
 	@staticmethod
-	def isWindowsServer():
+	def getWindowsVersion():
 		'''
-		Determines if the Windows host system is Windows Server
+		Returns the version information for the Windows host system as a semver instance
 		'''
-		key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion')
-		productName = winreg.QueryValueEx(key, 'ProductName')
-		winreg.CloseKey(key)
-		return 'Windows Server' in productName[0]
+		return semver.parse(platform.win32_ver()[1])
 	
 	@staticmethod
 	def getWindowsRelease():
@@ -54,30 +40,49 @@ class WindowsUtils(object):
 		return releaseId[0]
 	
 	@staticmethod
+	def isSupportedWindowsVersion():
+		'''
+		Verifies that the Windows host system is Windows 10 / Windows Server 2016 version 1607 or newer
+		
+		(1607 is the first build to support Windows containers, as per:
+		<https://docs.microsoft.com/en-us/virtualization/windowscontainers/deploy-containers/version-compatibility>)
+		'''
+		version = WindowsUtils.getWindowsVersion()
+		return version['major'] == 10 and version['patch'] >= 14393
+	
+	@staticmethod
+	def isWindowsServer():
+		'''
+		Determines if the Windows host system is Windows Server
+		'''
+		key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion')
+		productName = winreg.QueryValueEx(key, 'ProductName')
+		winreg.CloseKey(key)
+		return 'Windows Server' in productName[0]
+	
+	@staticmethod
+	def isInsiderPreview():
+		'''
+		Determines if the Windows host system is a Windows Insider preview build
+		'''
+		version = WindowsUtils.getWindowsVersion()
+		return version['patch'] > _latestReleaseBuild
+	
+	@staticmethod
 	def getReleaseBaseTag(release):
 		'''
 		Retrieves the tag for the Windows Server Core base image matching the specified Windows 10 / Windows Server release
 		'''
 		
+		# For Windows Insider preview builds, build the latest release tag
+		if WindowsUtils.isInsiderPreview():
+			return WindowsUtils._validTags[-1]
+		
 		# This lookup table is based on the list of valid tags from <https://hub.docker.com/r/microsoft/windowsservercore/>
 		return {
-			'1507': 'ltsc2016',
-			'1607': 'ltsc2016',
-			'1703': 'ltsc2016',
 			'1709': '1709',
 			'1803': '1803',
-			'1809': '1803',  # Temporary until the 1809 image becomes available
-			
-			# For Windows Insider preview builds, build the latest release tag
-			WindowsUtils._insiderSentinel: WindowsUtils._validTags[-1]
 		}.get(release, 'ltsc2016')
-	
-	@staticmethod
-	def isInsiderPreview(release):
-		'''
-		Determines if the specified Windows 10 / Windows Server release is a Windows Insider preview build
-		'''
-		return release == WindowsUtils._insiderSentinel
 	
 	@staticmethod
 	def getValidBaseTags():
@@ -96,6 +101,6 @@ class WindowsUtils(object):
 	@staticmethod
 	def isNewerBaseTag(older, newer):
 		'''
-		Determines if the base tag `newer` is actually newer than the base tag `older`
+		Determines if the base tag `newer` is chronologically newer than the base tag `older`
 		'''
 		return WindowsUtils._validTags.index(newer) > WindowsUtils._validTags.index(older)
