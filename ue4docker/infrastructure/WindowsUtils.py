7695e20dbb1abc8ae4f0b0af0e014f4fb7d63795
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-12-12 09:44:47 +1000

    Increase Windows image size limit to 500GB

--- ue4docker/infrastructure/WindowsUtils.py
+++ ue4docker/infrastructure/WindowsUtils.py
@@ -40,7 +40,7 @@ class WindowsUtils(object):
 		'''
 		Returns the minimum required image size limit (in GB) for Windows containers
 		'''
-		return 200.0
+		return 500.0
 	
 	@staticmethod
 	def minimumRequiredVersion():
