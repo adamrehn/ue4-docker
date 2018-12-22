9c1c608b242c98cec549ebe0ea2ba17de131d2a1
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-12-22 23:56:22 +1000

    Remove PDBs for AutomationTool under UE4.19

--- ue4docker/dockerfiles/ue4-minimal/windows/truncate-pdb-files.py
+++ ue4docker/dockerfiles/ue4-minimal/windows/truncate-pdb-files.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-import glob, os, sys
+import glob, json, os, sys
 
 # Logs a message to stderr
 def log(message):
 	print(message, file=sys.stderr)
 	sys.stderr.flush()
+
+# Reads the contents of a file
+def readFile(filename):
+	with open(filename, 'rb') as f:
+		return f.read().decode('utf-8')
+
+# Parse the UE4 version information
+version = json.loads(readFile(os.path.join(rootDir, 'Engine', 'Build', 'Build.version')))
 
 # Determine if we are preserving or truncating PDB files
 keepDebug = len(sys.argv) > 2 and sys.argv[2] == '1'
@@ -22,3 +30,13 @@ for pdbFile in pdbFiles:
 		os.truncate(pdbFile, 0)
 	except:
 		log('  Warning: failed to truncate PDB file {}.'.format(pdbFile))
+
+# Under UE4.19, we need to delete the PDB files for AutomationTool entirely, since truncated files cause issues
+if version['MinorVersion'] < 20:
+	pdbFiles = glob.glob(os.path.join(rootDir, 'Engine', 'Source', 'Programs', 'AutomationTool', '**', '*.pdb'), recursive=True)
+	for pdbFile in pdbFiles:
+		log('Removing PDB file {}...'.format(pdbFile))
+		try:
+			os.unlink(pdbFile)
+		except:
+			log('  Warning: failed to remove PDB file {}.'.format(pdbFile))
