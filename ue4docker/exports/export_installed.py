c7b755478f26fe9e99049591ccf8a2a4c08dfd1b
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-02-19 13:20:27 +1000

    Perform linker fixup on host when exporting Installed Builds

--- ue4docker/exports/export_installed.py
+++ ue4docker/exports/export_installed.py
@@ -1,5 +1,5 @@
 from ..infrastructure import DockerUtils, PackageUtils, SubprocessUtils
-import os, platform, subprocess, sys
+import os, platform, shutil, subprocess, sys
 
 # Import the `semver` package even when the conflicting `node-semver` package is present
 semver = PackageUtils.importFile('semver', os.path.join(PackageUtils.getPackageLocation('semver'), 'semver.py'))
@@ -9,6 +9,11 @@ def exportInstalledBuild(tag, destination, extraArgs):
 	# Verify that we are running under Linux
 	if platform.system() != 'Linux':
 		print('Error: Installed Builds can only be exported under Linux.', file=sys.stderr)
+		sys.exit(1)
+	
+	# Verify that the destination directory does not already exist
+	if os.path.exists(destination) == True:
+		print('Error: the destination directory already exists.', file=sys.stderr)
 		sys.exit(1)
 	
 	# Verify that the Installed Build in the specified image is at least 4.21.0
@@ -33,7 +38,17 @@ def exportInstalledBuild(tag, destination, extraArgs):
 	# Stop the container, irrespective of whether or not the export succeeded
 	container.stop()
 	
-	# Determine if the export succeeded
+	# If the export succeeded, regenerate the linker symlinks on the host system
+	if exportResult == 0:
+		print('Performing linker symlink fixup...')
+		subprocess.call([
+			sys.executable,
+			os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dockerfiles', 'ue4-source', 'linux', 'linker-fixup.py'),
+			os.path.join(destination, 'Engine/Extras/ThirdPartyNotUE/SDKs/HostLinux/Linux_x64'),
+			shutil.which('ld')
+		])
+	
+	# Report any failures
 	if exportResult != 0:
 		print('Error: failed to export Installed Build.', file=sys.stderr)
 		sys.exit(1)
