f0eda15d4c0376c440be882ea13ece004bc4003a
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-04-06 15:37:09 +1000

    Fix #27

--- ue4docker/infrastructure/BuildConfiguration.py
+++ ue4docker/infrastructure/BuildConfiguration.py
@@ -13,7 +13,8 @@ LINUX_BASE_IMAGES = {
 	'opengl': 'nvidia/opengl:1.0-glvnd-devel-ubuntu18.04',
 	'cudagl': {
 		'9.2':  'nvidia/cudagl:9.2-devel-ubuntu18.04',
-		'10.0': 'nvidia/cudagl:10.0-devel-ubuntu18.04'
+		'10.0': 'nvidia/cudagl:10.0-devel-ubuntu18.04',
+		'10.1': 'nvidia/cudagl:10.1-devel-ubuntu18.04'
 	}
 }
 
