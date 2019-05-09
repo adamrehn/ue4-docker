7046a50e7ea809b6f73a07ffd833799ed2f4cec5
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-05-09 20:53:56 +1000

    Use new MCR tag for .NET SDK base image

--- ue4docker/infrastructure/BuildConfiguration.py
+++ ue4docker/infrastructure/BuildConfiguration.py
@@ -170,7 +170,7 @@ class BuildConfiguration(object):
 		
 		# Store the tag for the base Windows Server Core image
 		self.basetag = self.args.basetag if self.args.basetag is not None else self.hostBasetag
-		self.baseImage = 'microsoft/dotnet-framework:4.7.2-sdk-windowsservercore-' + self.basetag
+		self.baseImage = 'mcr.microsoft.com/dotnet/framework/sdk:4.7.2-windowsservercore-' + self.basetag
 		self.prereqsTag = self.basetag
 		
 		# Verify that any user-specified base tag is valid
