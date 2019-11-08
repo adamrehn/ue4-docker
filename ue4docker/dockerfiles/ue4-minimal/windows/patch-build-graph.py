91f4381aa9c030ad6431f4c7f4dc0510219dbdd6
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-11-08 23:35:08 +1000

    Enable client and server targets by default in 4.23.0 onwards

--- ue4docker/dockerfiles/ue4-minimal/windows/patch-build-graph.py
+++ ue4docker/dockerfiles/ue4-minimal/windows/patch-build-graph.py
@@ -9,11 +9,25 @@ def writeFile(filename, data):
 	with open(filename, 'wb') as f:
 		f.write(data.encode('utf-8'))
 
-# Prevent HoloLens support from being enabled by default under Windows in 4.23.0 onwards
+# Read the build graph XML
 buildXml = sys.argv[1]
 code = readFile(buildXml)
+
+# Prevent HoloLens support from being enabled by default under Windows in 4.23.0 onwards
 code = code.replace(
 	'Option Name="WithHoloLens" Restrict="true|false" DefaultValue="$(DefaultWithWindows)"',
 	'Option Name="WithHoloLens" Restrict="true|false" DefaultValue="false"'
 )
+
+# Enable client and server targets by default in 4.23.0 onwards
+code = code.replace(
+	'Option Name="WithClient" Restrict="true|false" DefaultValue="false"',
+	'Option Name="WithClient" Restrict="true|false" DefaultValue="true"'
+)
+code = code.replace(
+	'Option Name="WithServer" Restrict="true|false" DefaultValue="false"',
+	'Option Name="WithServer" Restrict="true|false" DefaultValue="true"'
+)
+
+# Write the modified XML back to disk
 writeFile(buildXml, code)
