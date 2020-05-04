351d82126117b3cf6a22c3369e25abb5a89f279c
Adam Rehn <adam@adamrehn.com>
2020-05-04 17:56:28 +1000

    Run individual tests in temp directories to avoid Windows permission issues

--- ue4docker/tests/build-and-package.py
+++ ue4docker/tests/build-and-package.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, shutil, subprocess, time, ue4cli
+import os, subprocess, tempfile, ue4cli
 
 
 # Runs a command, raising an error if it returns a nonzero exit code
@@ -7,26 +7,16 @@ def run(command, **kwargs):
 	print('[RUN COMMAND] {} {}'.format(command, kwargs), flush=True)
 	return subprocess.run(command, check=True, **kwargs)
 
-# Repeatedly calls a function until it succeeds or the max number of retries has been reached
-def repeat(func, maxRetries=5, sleepTime=0.5):
-	for i in range(0, maxRetries):
-		try:
-			func()
-			break
-		except:
-			time.sleep(sleepTime)
-
 
 # Retrieve the short version string for the Engine
 manager = ue4cli.UnrealManagerFactory.create()
 version = manager.getEngineVersion('short')
 
-# Clone a simple C++ project and verify that we can build and package it
-repo = 'https://gitlab.com/ue4-test-projects/{}/BasicCxx.git'.format(version)
-projectDir = os.path.join(os.getcwd(), 'BasicCxx')
-run(['git', 'clone', '--depth=1', repo, projectDir])
-run(['ue4', 'package', 'Shipping'], cwd=projectDir)
-
-# Perform cleanup
-# (This can sometimes fail arbitrarily under Windows, so retry a few times if it does)
-repeat(lambda: shutil.rmtree(projectDir))
+# Create an auto-deleting temporary directory to work in
+with tempfile.TemporaryDirectory() as tempDir:
+	
+	# Clone a simple C++ project and verify that we can build and package it
+	repo = 'https://gitlab.com/ue4-test-projects/{}/BasicCxx.git'.format(version)
+	projectDir = os.path.join(tempDir, 'BasicCxx')
+	run(['git', 'clone', '--depth=1', repo, projectDir])
+	run(['ue4', 'package', 'Shipping'], cwd=projectDir)
--- ue4docker/tests/consume-external-deps.py
+++ ue4docker/tests/consume-external-deps.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
-import os, shutil, subprocess, time, ue4cli
+import os, subprocess, tempfile, ue4cli
 
 
 # Reads data from a file
 def read(filename):
 	with open(filename, 'rb') as f:
 		return f.read().decode('utf-8')
-
-# Repeatedly calls a function until it succeeds or the max number of retries has been reached
-def repeat(func, maxRetries=5, sleepTime=0.5):
-	for i in range(0, maxRetries):
-		try:
-			func()
-			break
-		except:
-			time.sleep(sleepTime)
 
 # Runs a command, raising an error if it returns a nonzero exit code
 def run(command, **kwargs):
@@ -31,36 +22,35 @@ def write(filename, data):
 manager = ue4cli.UnrealManagerFactory.create()
 version = manager.getEngineVersion('short')
 
-# Clone a simple C++ project
-repo = 'https://gitlab.com/ue4-test-projects/{}/BasicCxx.git'.format(version)
-projectDir = os.path.join(os.getcwd(), 'BasicCxx')
-run(['git', 'clone', '--depth=1', repo, projectDir])
-
-# Generate a code module to wrap our external dependencies
-sourceDir = os.path.join(projectDir, 'Source')
-run(['ue4', 'conan', 'boilerplate', 'WrapperModule'], cwd=sourceDir)
-
-# Add the wrapper module as a dependency of the project's main source code module
-rulesFile = os.path.join(sourceDir, 'BasicCxx', 'BasicCxx.Build.cs')
-rules = read(rulesFile)
-rules = rules.replace('PublicDependencyModuleNames.AddRange(new string[] {', 'PublicDependencyModuleNames.AddRange(new string[] { "WrapperModule", ')
-write(rulesFile, rules)
-
-# Add some dependencies to the module's conanfile.py
-moduleDir = os.path.join(sourceDir, 'WrapperModule')
-conanfile = os.path.join(moduleDir, 'conanfile.py')
-deps = read(conanfile)
-deps = deps.replace('pass', 'self._requireUnreal("zlib/ue4@adamrehn/{}")')
-write(conanfile, deps)
-
-# Verify that we can build the project with dynamically located dependencies
-run(['ue4', 'build'], cwd=projectDir)
-run(['ue4', 'clean'], cwd=projectDir)
-
-# Verify that we can build the project with precomputed dependency data
-run(['ue4', 'conan', 'precompute', 'host'], cwd=moduleDir)
-run(['ue4', 'build'], cwd=projectDir)
-
-# Perform cleanup
-# (This can sometimes fail arbitrarily under Windows, so retry a few times if it does)
-repeat(lambda: shutil.rmtree(projectDir))
+# Create an auto-deleting temporary directory to work in
+with tempfile.TemporaryDirectory() as tempDir:
+	
+	# Clone a simple C++ project
+	repo = 'https://gitlab.com/ue4-test-projects/{}/BasicCxx.git'.format(version)
+	projectDir = os.path.join(tempDir, 'BasicCxx')
+	run(['git', 'clone', '--depth=1', repo, projectDir])
+	
+	# Generate a code module to wrap our external dependencies
+	sourceDir = os.path.join(projectDir, 'Source')
+	run(['ue4', 'conan', 'boilerplate', 'WrapperModule'], cwd=sourceDir)
+	
+	# Add the wrapper module as a dependency of the project's main source code module
+	rulesFile = os.path.join(sourceDir, 'BasicCxx', 'BasicCxx.Build.cs')
+	rules = read(rulesFile)
+	rules = rules.replace('PublicDependencyModuleNames.AddRange(new string[] {', 'PublicDependencyModuleNames.AddRange(new string[] { "WrapperModule", ')
+	write(rulesFile, rules)
+	
+	# Add some dependencies to the module's conanfile.py
+	moduleDir = os.path.join(sourceDir, 'WrapperModule')
+	conanfile = os.path.join(moduleDir, 'conanfile.py')
+	deps = read(conanfile)
+	deps = deps.replace('pass', 'self._requireUnreal("zlib/ue4@adamrehn/{}")')
+	write(conanfile, deps)
+	
+	# Verify that we can build the project with dynamically located dependencies
+	run(['ue4', 'build'], cwd=projectDir)
+	run(['ue4', 'clean'], cwd=projectDir)
+	
+	# Verify that we can build the project with precomputed dependency data
+	run(['ue4', 'conan', 'precompute', 'host'], cwd=moduleDir)
+	run(['ue4', 'build'], cwd=projectDir)
