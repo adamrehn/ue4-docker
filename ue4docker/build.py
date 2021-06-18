adamrehn/ue4-docker

commit 88d686fba1bc128103cbd0f3fdb2f94e73f0e7d6

Add support for Visual Studio 2019 (#173)
resolves #172

ue4docker/build.py
@@ -116,6 +116,7 @@ def build():
 			logger.info('Host OS:                      {}'.format(WindowsUtils.systemString()), False)
 			logger.info('Memory limit:                 {}'.format('No limit' if config.memLimit is None else '{:.2f}GB'.format(config.memLimit)), False)
 			logger.info('Detected max image size:      {:.0f}GB'.format(DockerUtils.maxsize()), False)
+			logger.info('Visual Studio:                {}'.format(config.visualStudio), False)
 			logger.info('Directory to copy DLLs from:  {}\n'.format(config.dlldir), False)
 
 			# Verify that the specified base image tag is not a release that has reached End Of Life (EOL)
@@ -240,7 +241,10 @@ def build():
 			# (This is the only image that does not use any user-supplied tag suffix, since the tag always reflects any customisations)
 			prereqsArgs = ['--build-arg', 'BASEIMAGE=' + config.baseImage]
 			if config.containerPlatform == 'windows':
-				prereqsArgs = prereqsArgs + ['--build-arg', 'HOST_BUILD=' + str(WindowsUtils.getWindowsBuild())]
+				prereqsArgs = prereqsArgs + [
+					'--build-arg', 'HOST_BUILD=' + str(WindowsUtils.getWindowsBuild()),
+					'--build-arg', 'VISUAL_STUDIO_BUILD_NUMBER=' + config.visualStudioBuildNumber,
+				]
 			
 			# Build or pull the UE4 build prerequisites image (don't pull it if we're copying Dockerfiles to an output directory)
 			if config.layoutDir is None and config.pullPrerequisites == True:
@@ -282,7 +286,7 @@ def build():
 			# Build the minimal UE4 CI image, unless requested otherwise by the user
 			buildUe4Minimal = config.noMinimal == False
 			if buildUe4Minimal == True:
-				builder.build('ue4-minimal', mainTags, config.platformArgs + config.exclusionFlags + ue4BuildArgs)
+				builder.build('ue4-minimal', mainTags, config.platformArgs + config.exclusionFlags + ue4BuildArgs + config.uatBuildFlags)
 				builtImages.append('ue4-minimal')
 			else:
 				logger.info('User specified `--no-minimal`, skipping ue4-minimal image build.')

ue4docker/dockerfiles/ue4-minimal/windows/Dockerfile
@@ -27,8 +27,9 @@ RUN python C:\patch-build-graph.py C:\UnrealEngine\Engine\Build\InstalledEngineB
 
 # Create an Installed Build of the Engine
 ARG BUILD_DDC
+ARG USE_VS2019
 WORKDIR C:\UnrealEngine
-RUN .\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true -set:WithDDC=%BUILD_DDC% {{ buildgraph_args }} && `
+RUN .\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true -set:WithDDC=%BUILD_DDC% -set:VS2019=%USE_VS2019% {{ buildgraph_args }} && `
 	(if exist C:\UnrealEngine\LocalBuilds\InstalledDDC rmdir /s /q C:\UnrealEngine\LocalBuilds\InstalledDDC)
 
 # Determine if we are removing debug symbols and/or template projects in order to reduce the final container image size

ue4docker/infrastructure/BuildConfiguration.py
@@ -27,6 +27,23 @@ DEFAULT_CUDA_VERSION = '9.2'
 # The default memory limit (in GB) under Windows
 DEFAULT_MEMORY_LIMIT = 10.0
 
+class VisualStudio(object):
+	VS2017 = '2017'
+	VS2019 = '2019'
+
+	BuildNumbers = {
+		VS2017 : '15',
+		VS2019 : '16',
+	}
+
+	MinSupportedUnreal = {
+		# Unreal Engine 4.23.1 is the first that successfully builds with Visual Studio v16.3
+		# See https://github.com/EpicGames/UnrealEngine/commit/2510d4fd07a35ba5bff6ac2c7becaa6e8b7f11fa
+		#
+		# Unreal Engine 4.25 is the first that works with .NET SDK 4.7+
+		# See https://github.com/EpicGames/UnrealEngine/commit/5256eedbdef30212ab69fdf4c09e898098959683
+		VS2019 : semver.VersionInfo(4, 25)
+	}
 
 class ExcludedComponent(object):
 	'''
@@ -77,6 +94,7 @@ class BuildConfiguration(object):
 		parser.add_argument('--exclude', action='append', default=[], choices=[ExcludedComponent.DDC, ExcludedComponent.Debug, ExcludedComponent.Templates], help='Exclude the specified component (can be specified multiple times to exclude multiple components)')
 		parser.add_argument('--opt', action='append', default=[], help='Set an advanced configuration option (can be specified multiple times to specify multiple options)')
 		parser.add_argument('--cuda', default=None, metavar='VERSION', help='Add CUDA support as well as OpenGL support when building Linux containers')
+		parser.add_argument('--visual-studio', default=VisualStudio.VS2017, choices=VisualStudio.BuildNumbers.keys(), help='Specify Visual Studio Build Tools version to use for Windows containers')
 		parser.add_argument('-username', default=None, help='Specify the username to use when cloning the git repository')
 		parser.add_argument('-password', default=None, help='Specify the password or access token to use when cloning the git repository')
 		parser.add_argument('-repo', default=None, help='Set the custom git repository to clone when "custom" is specified as the release value')
@@ -201,6 +219,8 @@ class BuildConfiguration(object):
 			'--build-arg', 'EXCLUDE_DEBUG={}'.format(1 if ExcludedComponent.Debug in self.excludedComponents else 0),
 			'--build-arg', 'EXCLUDE_TEMPLATES={}'.format(1 if ExcludedComponent.Templates in self.excludedComponents else 0)
 		]
+
+		self.uatBuildFlags = []
 		
 		# If we're building Windows containers, generate our Windows-specific configuration settings
 		if self.containerPlatform == 'windows':
@@ -226,7 +246,19 @@ class BuildConfiguration(object):
 		hostSystem32 = os.path.join(os.environ['SystemRoot'], 'System32')
 		self.defaultDllDir = hostSysnative if os.path.exists(hostSysnative) else hostSystem32
 		self.dlldir = self.args.dlldir if self.args.dlldir is not None else self.defaultDllDir
-		
+
+		self.visualStudio = self.args.visual_studio
+
+		if not self.custom:
+			# Check whether specified Unreal Engine release is compatible with specified Visual Studio
+			vsMinSupportedUnreal = VisualStudio.MinSupportedUnreal.get(self.visualStudio, None)
+			if vsMinSupportedUnreal is not None and semver.VersionInfo.parse(self.release) < vsMinSupportedUnreal:
+				raise RuntimeError('specified version of Unreal Engine cannot be built with Visual Studio {}, oldest supported is {}'.format(self.visualStudio, vsMinSupportedUnreal))
+
+		self.visualStudioBuildNumber = VisualStudio.BuildNumbers[self.visualStudio]
+		# See https://github.com/EpicGames/UnrealEngine/commit/72585138472785e2ee58aab9950a7260275ee2ac
+		self.uatBuildFlags += ['--build-arg', 'USE_VS2019={}'.format('true' if self.visualStudio == VisualStudio.VS2019 else 'false')]
+
 		# Determine base tag for the Windows release of the host system
 		self.hostRelease = WindowsUtils.getWindowsRelease()
 		self.hostBasetag = WindowsUtils.getReleaseBaseTag(self.hostRelease)
@@ -234,7 +266,7 @@ class BuildConfiguration(object):
 		# Store the tag for the base Windows Server Core image
 		self.basetag = self.args.basetag if self.args.basetag is not None else self.hostBasetag
 		self.baseImage = 'mcr.microsoft.com/windows/servercore:' + self.basetag
-		self.prereqsTag = self.basetag
+		self.prereqsTag = self.basetag + '-vs' + self.visualStudio
 
 		# Verify that any user-specified base tag is valid
 		if WindowsUtils.isValidBaseTag(self.basetag) == False:

ue4docker/dockerfiles/ue4-build-prerequisites/windows/Dockerfile
@@ -61,4 +61,5 @@ RUN powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy 
 
 # Install the rest of our build prerequisites and clean up afterwards to minimise image size
 COPY buildtools-exitcode.py copy.py copy-pdbcopy.py install-prerequisites.bat C:\
-RUN C:\install-prerequisites.bat
+ARG VISUAL_STUDIO_BUILD_NUMBER
+RUN C:\install-prerequisites.bat %VISUAL_STUDIO_BUILD_NUMBER%

ue4docker/dockerfiles/ue4-build-prerequisites/windows/install-prerequisites.bat
@@ -10,24 +10,28 @@ call refreshenv
 @rem Forcibly disable the git credential manager
 git config --system credential.helper "" || goto :error
 
-@rem Install the Visual Studio 2017 Build Tools workloads and components we need
-@rem (Note that we use the Visual Studio 2019 installer here because the old installer now breaks, but explicitly install the VS2017 Build Tools)
+set VISUAL_STUDIO_BUILD_NUMBER=%~1
+
+@rem Install the Visual Studio Build Tools workloads and components we need
+@rem NOTE: We use the Visual Studio 2019 installer even for Visual Studio 2017 here because the old installer now breaks
+@rem NOTE: VS2019 Build Tools doesn't have 4.6.2 .NET SDK and what actually gets installed is 4.8
 curl --progress-bar -L "https://aka.ms/vs/16/release/vs_buildtools.exe" --output %TEMP%\vs_buildtools.exe || goto :error
 %TEMP%\vs_buildtools.exe --quiet --wait --norestart --nocache ^
 	--installPath C:\BuildTools ^
-	--channelUri "https://aka.ms/vs/15/release/channel" ^
-	--installChannelUri "https://aka.ms/vs/15/release/channel" ^
-	--channelId VisualStudio.15.Release ^
+	--channelUri "https://aka.ms/vs/%VISUAL_STUDIO_BUILD_NUMBER%/release/channel" ^
+	--installChannelUri "https://aka.ms/vs/%VISUAL_STUDIO_BUILD_NUMBER%/release/channel" ^
+	--channelId VisualStudio.%VISUAL_STUDIO_BUILD_NUMBER%.Release ^
 	--productId Microsoft.VisualStudio.Product.BuildTools ^
 	--locale en-US ^
 	--add Microsoft.VisualStudio.Workload.VCTools ^
 	--add Microsoft.VisualStudio.Workload.MSBuildTools ^
 	--add Microsoft.VisualStudio.Component.NuGet ^
 	--add Microsoft.VisualStudio.Component.Roslyn.Compiler ^
+	--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
 	--add Microsoft.VisualStudio.Component.Windows10SDK.17763 ^
 	--add Microsoft.Net.Component.4.5.TargetingPack ^
-	--add Microsoft.Net.Component.4.6.2.SDK ^
-	--add Microsoft.Net.Component.4.6.2.TargetingPack
+	--add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools
+
 python C:\buildtools-exitcode.py %ERRORLEVEL% || goto :error
 
 @rem Copy pdbcopy.exe to the expected location(s)

commit 40b764ce6a14f87d1110d6e9a06f0be6a97e9a6a

Do not pass VS2019 build graph arg to pre-VS2019 to avoid 'Unknown argument: VS2019' error
Broken by commit 88d686fba1bc128103cbd0f3fdb2f94e73f0e7d6

ue4docker/build.py
@@ -286,7 +286,8 @@ def build():
 			# Build the minimal UE4 CI image, unless requested otherwise by the user
 			buildUe4Minimal = config.noMinimal == False
 			if buildUe4Minimal == True:
-				builder.build('ue4-minimal', mainTags, config.platformArgs + config.exclusionFlags + ue4BuildArgs + config.uatBuildFlags)
+				buildGraphArg = ['--build-arg', 'BUILDGRAPH_ARGS=' + ' '.join(config.buildGraphArgs)]
+				builder.build('ue4-minimal', mainTags, config.platformArgs + config.exclusionFlags + ue4BuildArgs + buildGraphArg)
 				builtImages.append('ue4-minimal')
 			else:
 				logger.info('User specified `--no-minimal`, skipping ue4-minimal image build.')

ue4docker/dockerfiles/ue4-minimal/linux/Dockerfile
@@ -35,9 +35,9 @@ RUN python3 /tmp/patch-build-graph.py /home/ue4/UnrealEngine/Engine/Build/Instal
 RUN ./Engine/Build/BatchFiles/Linux/Build.sh UnrealHeaderTool Linux Development -SkipBuild
 
 # Create an Installed Build of the Engine
-ARG BUILD_DDC
+ARG BUILDGRAPH_ARGS
 WORKDIR /home/ue4/UnrealEngine
-RUN ./Engine/Build/BatchFiles/RunUAT.sh BuildGraph -target="Make Installed Build Linux" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true -set:WithDDC=$BUILD_DDC {{ buildgraph_args }} && \
+RUN ./Engine/Build/BatchFiles/RunUAT.sh BuildGraph -target="Make Installed Build Linux" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true ${BUILDGRAPH_ARGS} {{ buildgraph_args }} && \
 	rm -R -f /home/ue4/UnrealEngine/LocalBuilds/InstalledDDC
 
 # Determine if we are removing debug symbols and/or template projects in order to reduce the final container image size

ue4docker/dockerfiles/ue4-minimal/windows/Dockerfile
@@ -26,10 +26,9 @@ RUN python C:\patch-build-graph.py C:\UnrealEngine\Engine\Build\InstalledEngineB
 {% endif %}
 
 # Create an Installed Build of the Engine
-ARG BUILD_DDC
-ARG USE_VS2019
+ARG BUILDGRAPH_ARGS
 WORKDIR C:\UnrealEngine
-RUN .\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true -set:WithDDC=%BUILD_DDC% -set:VS2019=%USE_VS2019% {{ buildgraph_args }} && `
+RUN .\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:HostPlatformOnly=true %BUILDGRAPH_ARGS% {{ buildgraph_args }} && `
 	(if exist C:\UnrealEngine\LocalBuilds\InstalledDDC rmdir /s /q C:\UnrealEngine\LocalBuilds\InstalledDDC)
 
 # Determine if we are removing debug symbols and/or template projects in order to reduce the final container image size

ue4docker/infrastructure/BuildConfiguration.py
@@ -213,15 +213,14 @@ class BuildConfiguration(object):
 			raise RuntimeError('invalid value specified for the `credential_mode` option, valid values are {} when building {} containers'.format(validCredentialModes, self.containerPlatform.title()))
 		
 		# Generate our flags for keeping or excluding components
+		self.buildGraphArgs = [
+			'-set:WithDDC={}'.format('false' if ExcludedComponent.DDC in self.excludedComponents else 'true')
+		]
 		self.exclusionFlags = [
-			
-			'--build-arg', 'BUILD_DDC={}'.format('false' if ExcludedComponent.DDC in self.excludedComponents else 'true'),
 			'--build-arg', 'EXCLUDE_DEBUG={}'.format(1 if ExcludedComponent.Debug in self.excludedComponents else 0),
 			'--build-arg', 'EXCLUDE_TEMPLATES={}'.format(1 if ExcludedComponent.Templates in self.excludedComponents else 0)
 		]
 
-		self.uatBuildFlags = []
-		
 		# If we're building Windows containers, generate our Windows-specific configuration settings
 		if self.containerPlatform == 'windows':
 			self._generateWindowsConfig()
@@ -257,7 +256,10 @@ class BuildConfiguration(object):
 
 		self.visualStudioBuildNumber = VisualStudio.BuildNumbers[self.visualStudio]
 		# See https://github.com/EpicGames/UnrealEngine/commit/72585138472785e2ee58aab9950a7260275ee2ac
-		self.uatBuildFlags += ['--build-arg', 'USE_VS2019={}'.format('true' if self.visualStudio == VisualStudio.VS2019 else 'false')]
+		# Note: We must not pass VS2019 arg for older UE4 versions that didn't have VS2019 variable in their build graph xml.
+		# Otherwise, UAT errors out with "Unknown argument: VS2019".
+		if self.visualStudio != VisualStudio.VS2017:
+			self.buildGraphArgs += [f'-set:VS{self.visualStudio}=true']
 
 		# Determine base tag for the Windows release of the host system
 		self.hostRelease = WindowsUtils.getWindowsRelease()