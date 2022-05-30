adamrehn/ue4-docker

commit 4a1ca70073c1b2a3f8492375aa845e48570e74de

Always pass `-set:HostPlatformOnly=true` to build graph
Motivation: it just sets defaults and user is still able to enable platforms they need.

On the other hand, before this commit, `-set:HostPlatformOnly=true` would be lost if user specified *any* custom buildgraph_args

This is a follow-up to a6f15bf4daf037dd0323fee87aa25151ca8a6299

ue4docker/infrastructure/BuildConfiguration.py
@@ -474,7 +474,7 @@ class BuildConfiguration(object):
         )
 
         # Process any specified advanced configuration options (which we use directly as context values for the Jinja templating system)
-        self.opts = {"buildgraph_args": "-set:HostPlatformOnly=true"}
+        self.opts = {}
         for o in self.args.opt:
             if "=" in o:
                 key, value = o.split("=", 1)

ue4docker/dockerfiles/ue4-minimal/linux/Dockerfile
@@ -43,7 +43,9 @@ WORKDIR /home/ue4/UnrealEngine
 RUN ./Engine/Build/BatchFiles/RunUAT.sh BuildGraph \
     -target="Make Installed Build Linux" \
     -script=Engine/Build/InstalledEngineBuild.xml \
-    -set:WithDDC={% if excluded_components.ddc == true %}false{% else %}true{% endif %} {{ buildgraph_args }} && \
+    -set:HostPlatformOnly=true \
+    -set:WithDDC={% if excluded_components.ddc == true %}false{% else %}true{% endif %} \
+    {{ buildgraph_args }} && \
 	rm -R -f /home/ue4/UnrealEngine/LocalBuilds/InstalledDDC
 
 # Split out components (DDC, debug symbols, template projects) so they can be copied into the final container image as separate filesystem layers

ue4docker/dockerfiles/ue4-minimal/windows/Dockerfile
@@ -31,6 +31,7 @@ WORKDIR C:\UnrealEngine
 RUN .\Engine\Build\BatchFiles\RunUAT.bat BuildGraph `
     -target="Make Installed Build Win64" `
     -script=Engine/Build/InstalledEngineBuild.xml `
+    -set:HostPlatformOnly=true `
     -set:WithDDC={% if excluded_components.ddc == true %}false{% else %}true{% endif %} `
     {{ buildgraph_args }} && `
 	(if exist C:\UnrealEngine\LocalBuilds\InstalledDDC rmdir /s /q C:\UnrealEngine\LocalBuilds\InstalledDDC)

commit a9b9ace065da68a8d4a89edda179dd9d591808a6

Fix building on Windows with VS != 2017
Broken by 4a1ca70073c1b2a3f8492375aa845e48570e74de

ue4docker/infrastructure/BuildConfiguration.py
@@ -603,7 +603,10 @@ class BuildConfiguration(object):
         # Note: We must not pass VS2019 arg for older UE4 versions that didn't have VS2019 variable in their build graph xml.
         # Otherwise, UAT errors out with "Unknown argument: VS2019".
         if self.visualStudio != VisualStudio.VS2017:
-            self.opts["buildgraph_args"] += f" -set:VS{self.visualStudio}=true"
+            self.opts["buildgraph_args"] = (
+                self.opts.get("buildgraph_args", "")
+                + f" -set:VS{self.visualStudio}=true"
+            )
 
         # Determine base tag for the Windows release of the host system
         self.hostBasetag = WindowsUtils.getHostBaseTag()