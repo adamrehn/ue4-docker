47aaf66a82a14657b22a3cd2badb8b97a9b80677
Nigel George <nigel.george@shiroikuma.co.uk>
2022-12-10 09:56:01 +0900

    fix(package): patch filters to exclude hololens due to package error

--- ue4docker/dockerfiles/ue4-source/windows/patch-broken-releases.py
+++ ue4docker/dockerfiles/ue4-source/windows/patch-broken-releases.py
@@ -53,3 +53,30 @@ if (
             print("PATCHED {}:\n\n{}".format(gitdepsFile, gitdepsXml), file=sys.stderr)
         else:
             print("PATCHED {}".format(gitdepsFile), file=sys.stderr)
+
+
+# Determine if we are building UE 5.1.0
+if (
+    versionDetails["MajorVersion"] == 5
+    and versionDetails["MinorVersion"] == 1
+    and versionDetails["PatchVersion"] == 0
+):
+
+    # Hack InstalledEngineFilters.xml with the changes from CL 23300641
+    # (See: <https://github.com/EpicGames/UnrealEngine/commit/ae9de79b7012fc33df355c8dbfe5096b94545e3c>)
+    buildFile = join(engineRoot, "Engine", "Build", "InstalledEngineFilters.xml")
+    buildXml = readFile(buildFile)
+    if 'HoloLens.Automation.json' not in buildXml:
+
+        buildXml = buildXml.replace(
+            '<Property Name="CopyWin64CsToolsExceptions">',
+            '<Property Name="CopyWin64CsToolsExceptions">\n'
+			+ '            Engine\Saved\CsTools\Engine\Intermediate\ScriptModules\HoloLens.Automation.json\n',
+        )
+
+        writeFile(buildFile, buildXml)
+
+        if verboseOutput == True:
+            print("PATCHED {}:\n\n{}".format(buildFile, buildFile), file=sys.stderr)
+        else:
+            print("PATCHED {}".format(buildFile), file=sys.stderr)
