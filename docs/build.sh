ad058965c39bb3830d7352cc51ee6cb5647f1d5f
Marat Radchenko <marat@slonopotamus.org>
2022-12-12 15:32:48 +0300

    Minor fixes to manpages

--- docs/build.sh
+++ docs/build.sh
@@ -10,6 +10,17 @@ function docker_asciidoctor() {
     docker run --rm --user "$(id -u):$(id -g)" --volume "${project_dir}:/project/" asciidoctor/docker-asciidoctor "$@"
 }
 
+# HTML
 docker_asciidoctor asciidoctor -a linkcss /project/docs/index.adoc -D /project/build/gh-pages/
+
+# PDF
 docker_asciidoctor asciidoctor-pdf /project/docs/index.adoc -o /project/build/gh-pages/ue4-docker.pdf
+
+# EPUB
 docker_asciidoctor asciidoctor-epub3 /project/docs/index.adoc -o /project/build/gh-pages/ue4-docker.epub
+
+# Manpages
+for f in docs/ue4-docker-*.adoc
+do
+  docker_asciidoctor asciidoctor -b manpage "/project/${f}" -D /project/build/gh-pages/
+done
