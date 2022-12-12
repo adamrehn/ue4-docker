6a90510cfb04dbf0d365e4f52d35ffe1df17310d
Marat Radchenko <marat@slonopotamus.org>
2022-12-12 12:13:03 +0300

    Quick and dirty pipeline for docs publishing

---
+++ docs/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+project_dir="$(git rev-parse --show-toplevel)"
+
+function docker_asciidoctor() {
+    docker run --rm --user "$(id -u):$(id -g)" --volume "${project_dir}:/project/" asciidoctor/docker-asciidoctor "$@"
+}
+
+docker_asciidoctor asciidoctor /project/docs/index.adoc -o /project/build/gh-pages/index.html
+docker_asciidoctor asciidoctor-pdf /project/docs/index.adoc -o /project/build/gh-pages/ue4-docker.pdf
+docker_asciidoctor asciidoctor-epub3 /project/docs/index.adoc -o /project/build/gh-pages/ue4-docker.epub
