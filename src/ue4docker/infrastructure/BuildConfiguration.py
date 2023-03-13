adamrehn/ue4-docker

commit ce89fcd226e2e43fd8b29df68de35e4573c65df7

Drop dependency on `semver` package (#298)
1. We already use `packaging` because it is less strict
2. This allows to drop hacks around `node-semver`

src/ue4docker/infrastructure/BuildConfiguration.py
@@ -1,13 +1,12 @@
-from .DockerUtils import DockerUtils
-from .PackageUtils import PackageUtils
-from .WindowsUtils import WindowsUtils
-import humanfriendly, json, os, platform, random
-from pkg_resources import parse_version
+import json
+import platform
+import random
 
-# Import the `semver` package even when the conflicting `node-semver` package is present
-semver = PackageUtils.importFile(
-    "semver", os.path.join(PackageUtils.getPackageLocation("semver"), "semver.py")
-)
+import humanfriendly
+from packaging.version import Version, InvalidVersion
+
+from .DockerUtils import DockerUtils
+from .WindowsUtils import WindowsUtils
 
 # The default Unreal Engine git repository
 DEFAULT_GIT_REPO = "https://github.com/EpicGames/UnrealEngine.git"
@@ -55,17 +54,17 @@ class VisualStudio(object):
 
     SupportedSince = {
         # We do not support versions older than 4.20
-        VS2017: semver.VersionInfo(4.20),
+        VS2017: Version("4.20"),
         # Unreal Engine 4.23.1 is the first that successfully builds with Visual Studio v16.3
         # See https://github.com/EpicGames/UnrealEngine/commit/2510d4fd07a35ba5bff6ac2c7becaa6e8b7f11fa
         #
         # Unreal Engine 4.25 is the first that works with .NET SDK 4.7+
         # See https://github.com/EpicGames/UnrealEngine/commit/5256eedbdef30212ab69fdf4c09e898098959683
-        VS2019: semver.VersionInfo(4, 25),
-        VS2022: semver.VersionInfo(5, 0, 0),
+        VS2019: Version("4.25"),
+        VS2022: Version("5.0.0"),
     }
 
-    UnsupportedSince = {VS2017: semver.VersionInfo(5, 0)}
+    UnsupportedSince = {VS2017: Version("5.0")}
 
 
 class ExcludedComponent(object):
@@ -406,16 +405,13 @@ class BuildConfiguration(object):
             else:
                 # Validate the specified version string
                 try:
-                    ue4Version = semver.parse(self.args.release)
-                    if (
-                        ue4Version["major"] not in [4, 5]
-                        or ue4Version["prerelease"] != None
-                    ):
+                    ue4Version = Version(self.args.release)
+                    if ue4Version.major not in [4, 5] or ue4Version.pre != 0:
                         raise Exception()
-                    self.release = semver.format_version(
-                        ue4Version["major"], ue4Version["minor"], ue4Version["patch"]
+                    self.release = (
+                        f"{ue4Version.major}.{ue4Version.minor}.{ue4Version.micro}"
                     )
-                except:
+                except InvalidVersion:
                     raise RuntimeError(
                         'invalid Unreal Engine release number "{}", full semver format required (e.g. "4.20.0")'.format(
                             self.args.release
@@ -545,11 +541,7 @@ class BuildConfiguration(object):
             if ExcludedComponent.Debug not in self.excludedComponents:
                 logger.warning("Warning: You didn't pass --exclude debug", False)
                 warn20GiB = True
-            if (
-                self.release
-                and not self.custom
-                and semver.VersionInfo.parse(self.release) >= semver.VersionInfo(5, 0)
-            ):
+            if self.release and not self.custom and Version(self.release).major >= 5:
                 logger.warning("Warning: You're building Unreal Engine 5", False)
                 warn20GiB = True
 
@@ -592,10 +584,7 @@ class BuildConfiguration(object):
         if self.release is not None and not self.custom:
             # Check whether specified Unreal Engine release is compatible with specified Visual Studio
             supportedSince = VisualStudio.SupportedSince.get(self.visualStudio, None)
-            if (
-                supportedSince is not None
-                and semver.VersionInfo.parse(self.release) < supportedSince
-            ):
+            if supportedSince is not None and Version(self.release) < supportedSince:
                 raise RuntimeError(
                     "specified version of Unreal Engine is too old for Visual Studio {}".format(
                         self.visualStudio
@@ -607,7 +596,7 @@ class BuildConfiguration(object):
             )
             if (
                 unsupportedSince is not None
-                and semver.VersionInfo.parse(self.release) >= unsupportedSince
+                and Version(self.release) >= unsupportedSince
             ):
                 raise RuntimeError(
                     "Visual Studio {} is too old for specified version of Unreal Engine".format(
@@ -648,9 +637,9 @@ class BuildConfiguration(object):
         else:
             # If we are able to use process isolation mode then use it, otherwise use Hyper-V isolation mode
             differentKernels = self.basetag != self.hostBasetag
-            dockerSupportsProcess = parse_version(
+            dockerSupportsProcess = Version(
                 DockerUtils.version()["Version"]
-            ) >= parse_version("18.09.0")
+            ) >= Version("18.09.0")
             if not differentKernels and dockerSupportsProcess:
                 self.isolation = "process"
             else:

pyproject.toml
@@ -30,7 +30,6 @@ dependencies = [
     "packaging>=19.1",
     "psutil",
     "requests",
-    "semver>=2.7.9,<3.0.0",
     "termcolor",
 ]
 

src/ue4docker/infrastructure/DarwinUtils.py
@@ -1,17 +1,18 @@
-from packaging import version
-import os, platform
+import platform
+
+from packaging.version import Version
 
 
 class DarwinUtils(object):
     @staticmethod
-    def minimumRequiredVersion():
+    def minimumRequiredVersion() -> Version:
         """
         Returns the minimum required version of macOS, which is 10.10.3 Yosemite
 
         (10.10.3 is the minimum required version for Docker for Mac, as per:
         <https://store.docker.com/editions/community/docker-ce-desktop-mac>)
         """
-        return "10.10.3"
+        return Version("10.10.3")
 
     @staticmethod
     def systemString():
@@ -23,17 +24,15 @@ class DarwinUtils(object):
         )
 
     @staticmethod
-    def getMacOsVersion():
+    def getMacOsVersion() -> Version:
         """
         Returns the version number for the macOS host system
         """
-        return platform.mac_ver()[0]
+        return Version(platform.mac_ver()[0])
 
     @staticmethod
     def isSupportedMacOsVersion():
         """
         Verifies that the macOS host system meets our minimum version requirements
         """
-        return version.parse(DarwinUtils.getMacOsVersion()) >= version.parse(
-            DarwinUtils.minimumRequiredVersion()
-        )
+        return DarwinUtils.getMacOsVersion() >= DarwinUtils.minimumRequiredVersion()

src/ue4docker/infrastructure/GlobalConfiguration.py
@@ -1,6 +1,7 @@
-from pkg_resources import parse_version
-import os, requests
+import os
 
+import requests
+from packaging.version import Version
 
 # The default namespace for our tagged container images
 DEFAULT_TAG_NAMESPACE = "adamrehn"
@@ -17,7 +18,7 @@ class GlobalConfiguration(object):
         Queries PyPI to determine the latest available release of ue4-docker
         """
         releases = [
-            parse_version(release)
+            Version(release)
             for release in requests.get("https://pypi.org/pypi/ue4-docker/json").json()[
                 "releases"
             ]

src/ue4docker/infrastructure/PackageUtils.py
@@ -1,20 +1,0 @@
-import importlib.util, pkg_resources
-
-
-class PackageUtils(object):
-    @staticmethod
-    def getPackageLocation(package):
-        """
-        Attempts to retrieve the filesystem location for the specified Python package
-        """
-        return pkg_resources.get_distribution(package).location
-
-    @staticmethod
-    def importFile(moduleName, filePath):
-        """
-        Directly imports a Python module from a source file
-        """
-        spec = importlib.util.spec_from_file_location(moduleName, filePath)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        return module

src/ue4docker/infrastructure/WindowsUtils.py
@@ -1,5 +1,5 @@
 from .DockerUtils import DockerUtils
-from pkg_resources import parse_version
+from packaging.version import Version
 import platform, sys
 from typing import Optional
 
@@ -94,11 +94,10 @@ class WindowsUtils(object):
         Determines if host Windows version is one with bugs that make it unsuitable for use
         (defaults to checking the host OS release if one is not specified)
         """
-        dockerVersion = parse_version(DockerUtils.version()["Version"])
+        dockerVersion = Version(DockerUtils.version()["Version"])
         build = WindowsUtils.getWindowsBuild()
-        return (
-            build in WindowsUtils._blacklistedHosts
-            and dockerVersion < parse_version("19.03.6")
+        return build in WindowsUtils._blacklistedHosts and dockerVersion < Version(
+            "19.03.6"
         )
 
     @staticmethod

src/ue4docker/infrastructure/__init__.py
@@ -8,7 +8,6 @@ from .ImageBuilder import ImageBuilder
 from .ImageCleaner import ImageCleaner
 from .Logger import Logger
 from .NetworkUtils import NetworkUtils
-from .PackageUtils import PackageUtils
 from .PrettyPrinting import PrettyPrinting
 from .ResourceMonitor import ResourceMonitor
 from .SubprocessUtils import SubprocessUtils

commit 739d4dd5e1e132ea9c0dfe60bfb7297953577eb3

resolves #303 fix `ue4-docker build` with non-custom engine version
Broken by ce89fcd226e2e43fd8b29df68de35e4573c65df7

src/ue4docker/infrastructure/BuildConfiguration.py
@@ -406,8 +406,8 @@ class BuildConfiguration(object):
                 # Validate the specified version string
                 try:
                     ue4Version = Version(self.args.release)
-                    if ue4Version.major not in [4, 5] or ue4Version.pre != 0:
-                        raise Exception()
+                    if ue4Version.major not in [4, 5] or ue4Version.pre is not None:
+                        raise Exception(f"unsupported engine version: {ue4Version}")
                     self.release = (
                         f"{ue4Version.major}.{ue4Version.minor}.{ue4Version.micro}"
                     )