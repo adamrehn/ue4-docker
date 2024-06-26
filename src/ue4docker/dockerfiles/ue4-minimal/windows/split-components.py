adamrehn/ue4-docker

commit 3607cb128140baa6c5115675458bf712f5bc7acf

Follow Epic's logic for removing debug symbols (#346)
There are `pdb` files, not prefaced with `UnrealEngine`, which are expected to be present when creating a shipping build.

src/ue4docker/dockerfiles/ue4-minimal/windows/split-components.py
@@ -48,7 +48,7 @@ ddc = [join(rootDir, "Engine", "DerivedDataCache", "Compressed.ddp")]
 extractComponent(rootDir, outputDir, "DDC", "Derived Data Cache (DDC)", ddc)
 
 # Extract debug symbols
-symbolFiles = glob.glob(join(rootDir, "**", "*.pdb"), recursive=True)
+symbolFiles = glob.glob(join(rootDir, "**", "*UnrealEditor*.pdb"), recursive=True)
 extractComponent(rootDir, outputDir, "DebugSymbols", "debug symbols", symbolFiles)
 
 # Extract template projects and samples

commit 4e53548b79ec11d7b5646730d33f8ca59d2417ab

Fix 4.27 Windows breakage introduced in #346

src/ue4docker/dockerfiles/ue4-minimal/windows/split-components.py
@@ -48,7 +48,7 @@ ddc = [join(rootDir, "Engine", "DerivedDataCache", "Compressed.ddp")]
 extractComponent(rootDir, outputDir, "DDC", "Derived Data Cache (DDC)", ddc)
 
 # Extract debug symbols
-symbolFiles = glob.glob(join(rootDir, "**", "*UnrealEditor*.pdb"), recursive=True)
+symbolFiles = glob.glob(join(rootDir, "**", "*U*Editor*.pdb"), recursive=True)
 extractComponent(rootDir, outputDir, "DebugSymbols", "debug symbols", symbolFiles)
 
 # Extract template projects and samples