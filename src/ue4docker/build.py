adamrehn/ue4-docker

commit 9d0d2aa6de53b2f9eefbb5cdb34f1dfad91097ab

Drop engines older than 4.27 (#370)

src/ue4docker/build.py
@@ -465,34 +465,38 @@ def build():
                     "--build-arg",
                     "VERBOSE_OUTPUT={}".format("1" if config.verbose == True else "0"),
                 ]
-                builder.build_builtin_image(
-                    "ue4-source",
-                    mainTags,
-                    commonArgs + config.platformArgs + ue4SourceArgs + credentialArgs,
-                    secrets=secrets,
-                )
-                builtImages.append("ue4-source")
-            else:
-                logger.info("Skipping ue4-source image build.")
 
-            if config.buildTargets["minimal"]:
-                ue4BuildArgs = prereqConsumerArgs + [
-                    "--build-arg",
-                    "TAG={}".format(mainTags[1]),
-                ]
-
-            # Build the minimal UE4 CI image, unless requested otherwise by the user
-            if config.buildTargets["minimal"]:
-                minimalArgs = (
+                changelistArgs = (
                     ["--build-arg", "CHANGELIST={}".format(config.changelist)]
                     if config.changelist is not None
                     else []
                 )
 
                 builder.build_builtin_image(
+                    "ue4-source",
+                    mainTags,
+                    commonArgs
+                    + config.platformArgs
+                    + ue4SourceArgs
+                    + credentialArgs
+                    + changelistArgs,
+                    secrets=secrets,
+                )
+                builtImages.append("ue4-source")
+            else:
+                logger.info("Skipping ue4-source image build.")
+
+            # Build the minimal UE4 CI image, unless requested otherwise by the user
+            if config.buildTargets["minimal"]:
+                minimalArgs = prereqConsumerArgs + [
+                    "--build-arg",
+                    "TAG={}".format(mainTags[1]),
+                ]
+
+                builder.build_builtin_image(
                     "ue4-minimal",
                     mainTags,
-                    commonArgs + config.platformArgs + ue4BuildArgs + minimalArgs,
+                    commonArgs + config.platformArgs + minimalArgs,
                 )
                 builtImages.append("ue4-minimal")
             else:

src/ue4docker/dockerfiles/ue4-build-prerequisites/windows/install-prerequisites.ps1
@@ -56,26 +56,9 @@ Copy-Item -Path "*\x64\vulkan-1.dll" -Destination C:\Windows\System32\
 
 $visual_studio_build = $args[0]
 
-# Use the latest available Windows SDK. The motivation behind this is:
-# 1. Newer SDKs allow developers to use newer APIs. Developers can guard that API usage with runtime version checks if they want to continue to support older Windows releases.
-# 2. Unreal Engine slowly moves to newer Windows SDK. 4.27.0 no longer compiles with SDKs older than 18362 and even if it will be fixed in 4.27.x,
-#    this is just a question of a time when older SDKs support will be dropped completely
-# 3. UE5 doesn't support VS2017 at all, so in the future that argument for continuing to use Windows SDK 17763 from VS2017 era will be weaker and weaker.
-#
-# We can't use newer SDK for VS2017 that is used to compile older engines because 18362 SDK support was only added in UE-4.23.
-#
-# See https://github.com/adamrehn/ue4-docker/issues/192
-# See https://forums.unrealengine.com/t/ndis_miniport_major_version-is-not-defined-error/135058
-# See https://github.com/EpicGames/UnrealEngine/blame/4.23.0-release/Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs#L1822-L1823
-# See https://github.com/EpicGames/UnrealEngine/commit/ecc4872c3269e75a24adc40734cc8bcc9bbed1ca
-# See https://udn.unrealengine.com/s/question/0D54z000079HcjJCAS/d3d12h427-error-c4668-winapipartitiongames-is-not-defined-as-a-preprocessor-macro-replacing-with-0-for-ifelif
-#
-# Keywords for Google:
-# error C4668: 'NDIS_MINIPORT_MAJOR_VERSION' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif
-# d3d12.h(427): error C4668: 'WINAPI_PARTITION_GAMES' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
 if ($visual_studio_build -eq "15")
 {
-    $windows_sdk_version = 17763
+    $windows_sdk_version = 18362
 }
 else
 {

src/ue4docker/dockerfiles/ue4-minimal/linux/Dockerfile
@@ -7,28 +7,8 @@ ARG PREREQS_TAG
 FROM ${NAMESPACE}/ue4-source:${TAG}-${PREREQS_TAG} AS builder
 {% endif %}
 
-# Set the changelist number in Build.version to ensure our Build ID is generated correctly
-ARG CHANGELIST
-COPY set-changelist.py /tmp/set-changelist.py
-RUN python3 /tmp/set-changelist.py /home/ue4/UnrealEngine/Engine/Build/Build.version $CHANGELIST
-
 # Remove the .git directory to disable UBT `git status` calls and speed up the build process
 RUN rm -rf /home/ue4/UnrealEngine/.git
-
-{% if (not disable_all_patches) and (not disable_opengl_patch) %}
-# Enable the OpenGL RHI for Engine versions where it is present but deprecated
-COPY enable-opengl.py /tmp/enable-opengl.py
-RUN python3 /tmp/enable-opengl.py /home/ue4/UnrealEngine/Engine/Config/BaseEngine.ini
-{% endif %}
-
-{% if (not disable_all_patches) and (not disable_buildgraph_patches) %}
-COPY patch-filters-xml.py /tmp/patch-filters-xml.py
-RUN python3 /tmp/patch-filters-xml.py /home/ue4/UnrealEngine/Engine/Build/InstalledEngineFilters.xml
-
-# Patch the default settings in InstalledEngineBuild.xml and increase the output verbosity of the DDC generation step
-COPY patch-build-graph.py /tmp/patch-build-graph.py
-RUN python3 /tmp/patch-build-graph.py /home/ue4/UnrealEngine/Engine/Build/InstalledEngineBuild.xml /home/ue4/UnrealEngine/Engine/Build/Build.version
-{% endif %}
 
 # Ensure UBT is built before we create the Installed Build, since Build.sh explicitly sets the
 # target .NET Framework version, whereas InstalledEngineBuild.xml just uses the system default,
@@ -41,7 +21,9 @@ RUN ./Engine/Build/BatchFiles/RunUAT.sh BuildGraph \
 	-target="Make Installed Build Linux" \
 	-script=Engine/Build/InstalledEngineBuild.xml \
 	-set:HostPlatformOnly=true \
+	-set:WithClient=true \
 	-set:WithDDC={% if excluded_components.ddc == true %}false{% else %}true{% endif %} \
+	-set:WithServer=true \
 	{{ buildgraph_args }} && \
 	rm -R -f /home/ue4/UnrealEngine/LocalBuilds/InstalledDDC
 
@@ -53,23 +35,6 @@ RUN ./Engine/Build/BatchFiles/Linux/Build.sh UnrealVersionSelector Linux Shippin
 # (Note also that custom Build IDs are supported by Unreal Engine 5.3.1 and newer, and older versions will just use a GUID as the Build ID)
 RUN cp /home/ue4/UnrealEngine/LocalBuilds/Engine/Linux/Engine/Build/InstalledBuild.txt /home/ue4/UnrealEngine/Engine/Build/InstalledBuild.txt && \
 	./Engine/Binaries/Linux/UnrealVersionSelector-Linux-Shipping -register -unattended
-
-{% if (not disable_all_patches) and (not disable_target_patches) %}
-# Ensure Client and Server targets have their `PlatformType` field set correctly in BaseEngine.ini
-COPY fix-targets.py /tmp/fix-targets.py
-RUN python3 /tmp/fix-targets.py /home/ue4/UnrealEngine/LocalBuilds/Engine/Linux/Engine/Config/BaseEngine.ini
-{% endif %}
-
-{% if (not disable_all_patches) and (not disable_unrealpak_copy) %}
-# Some versions of the Engine fail to include UnrealPak in the Installed Build, so copy it manually
-RUN cp ./Engine/Binaries/Linux/UnrealPak ./LocalBuilds/Engine/Linux/Engine/Binaries/Linux/UnrealPak
-{% endif %}
-
-{% if (not disable_all_patches) and (not disable_toolchain_copy) %}
-# Ensure the bundled toolchain included in 4.20.0 and newer is copied to the Installed Build
-COPY --chown=ue4:ue4 copy-toolchain.py /tmp/copy-toolchain.py
-RUN python3 /tmp/copy-toolchain.py /home/ue4/UnrealEngine
-{% endif %}
 
 {% if enable_ushell %}
 # Ensure ushell is copied to the Installed Build

src/ue4docker/dockerfiles/ue4-minimal/linux/copy-toolchain.py
@@ -1,43 +1,0 @@
-#!/usr/bin/env python3
-from os.path import basename, dirname, exists, join, relpath
-import glob, shutil, sys
-
-# Determine the root directory for the source build and the Installed Build
-sourceRoot = sys.argv[1]
-installedRoot = join(sourceRoot, "LocalBuilds", "Engine", "Linux")
-
-# Locate the bundled toolchain and copy it to the Installed Build
-sdkGlob = join(
-    sourceRoot,
-    "Engine",
-    "Extras",
-    "ThirdPartyNotUE",
-    "SDKs",
-    "HostLinux",
-    "Linux_x64",
-    "*",
-    "x86_64-unknown-linux-gnu",
-)
-for bundled in glob.glob(sdkGlob):
-    # Extract the root path for the toolchain
-    toolchain = dirname(bundled)
-
-    # Print progress output
-    print(
-        'Copying bundled toolchain "{}" to Installed Build...'.format(
-            basename(toolchain)
-        ),
-        file=sys.stderr,
-    )
-    sys.stderr.flush()
-
-    # Perform the copy
-    dest = join(installedRoot, relpath(toolchain, sourceRoot))
-    if exists(dest) == True:
-        print(
-            "Destination toolchain already exists: {}".format(dest),
-            file=sys.stderr,
-            flush=True,
-        )
-    else:
-        shutil.copytree(toolchain, dest)

src/ue4docker/dockerfiles/ue4-minimal/linux/enable-opengl.py
@@ -1,19 +1,0 @@
-#!/usr/bin/env python3
-import os, sys
-
-
-def readFile(filename):
-    with open(filename, "rb") as f:
-        return f.read().decode("utf-8")
-
-
-def writeFile(filename, data):
-    with open(filename, "wb") as f:
-        f.write(data.encode("utf-8"))
-
-
-# Enable the OpenGL RHI for Engine versions where it is present but deprecated
-iniFile = sys.argv[1]
-config = readFile(iniFile)
-config = config.replace("; +TargetedRHIs=GLSL_430", "+TargetedRHIs=GLSL_430")
-writeFile(iniFile, config)

src/ue4docker/dockerfiles/ue4-minimal/linux/fix-targets.py
@@ -1,23 +1,0 @@
-#!/usr/bin/env python3
-import os, re, sys
-
-
-def readFile(filename):
-    with open(filename, "rb") as f:
-        return f.read().decode("utf-8")
-
-
-def writeFile(filename, data):
-    with open(filename, "wb") as f:
-        f.write(data.encode("utf-8"))
-
-
-# Ensure the `PlatformType` field is set correctly for Client and Server targets in BaseEngine.ini
-iniFile = sys.argv[1]
-config = readFile(iniFile)
-config = re.sub(
-    'PlatformType="Game", RequiredFile="(.+UE4(Client|Server).*\\.target)"',
-    'PlatformType="\\2", RequiredFile="\\1"',
-    config,
-)
-writeFile(iniFile, config)

src/ue4docker/dockerfiles/ue4-minimal/linux/patch-build-graph.py
@@ -1,57 +1,0 @@
-#!/usr/bin/env python3
-import json, os, sys
-
-
-def readFile(filename):
-    with open(filename, "rb") as f:
-        return f.read().decode("utf-8")
-
-
-def writeFile(filename, data):
-    with open(filename, "wb") as f:
-        f.write(data.encode("utf-8"))
-
-
-# Read the build graph XML
-buildXml = sys.argv[1]
-code = readFile(buildXml)
-
-# Read the UE4 version information
-versionFile = sys.argv[2]
-versionData = json.loads(readFile(versionFile))
-
-# Add verbose output flags to the `BuildDerivedDataCache` command
-code = code.replace(
-    'Command Name="BuildDerivedDataCache" Arguments="',
-    'Command Name="BuildDerivedDataCache" Arguments="-Verbose -AllowStdOutLogVerbosity ',
-)
-
-# Disable AArch64 by default (enabled since 4.24.0)
-code = code.replace(
-    'Property Name="DefaultWithLinuxAArch64" Value="true"',
-    'Property Name="DefaultWithLinuxAArch64" Value="false"',
-)
-# AArch64 was renamed to Arm64 in UE-5.0, so also disable it
-code = code.replace(
-    'Property Name="DefaultWithLinuxArm64" Value="true"',
-    'Property Name="DefaultWithLinuxArm64" Value="false"',
-)
-
-# Enable client and server targets by default in 4.23.0 onwards, except for 4.24.0 - 4.24.2 where Linux server builds fail
-# (See <https://issues.unrealengine.com/issue/UE-87878> for details of the bug and its fix)
-if (
-    versionData["MajorVersion"] != 4
-    or versionData["MinorVersion"] != 24
-    or versionData["PatchVersion"] >= 3
-):
-    code = code.replace(
-        'Option Name="WithClient" Restrict="true|false" DefaultValue="false"',
-        'Option Name="WithClient" Restrict="true|false" DefaultValue="true"',
-    )
-    code = code.replace(
-        'Option Name="WithServer" Restrict="true|false" DefaultValue="false"',
-        'Option Name="WithServer" Restrict="true|false" DefaultValue="true"',
-    )
-
-# Write the modified XML back to disk
-writeFile(buildXml, code)

src/ue4docker/dockerfiles/ue4-minimal/linux/patch-filters-xml.py
@@ -1,26 +1,0 @@
-#!/usr/bin/env python3
-import os, sys
-
-
-def readFile(filename):
-    with open(filename, "rb") as f:
-        return f.read().decode("utf-8")
-
-
-def writeFile(filename, data):
-    with open(filename, "wb") as f:
-        f.write(data.encode("utf-8"))
-
-
-filtersXml = sys.argv[1]
-code = readFile(filtersXml)
-
-# Add missing SetupDotnet.sh in Unreal Engine 5.0.0-early-access-1
-# See https://github.com/adamrehn/ue4-docker/issues/171#issuecomment-853918412
-# and https://github.com/EpicGames/UnrealEngine/commit/a18824057e6cd490750a10b59af29ca10b3d67d9
-dotnet = "Engine/Binaries/ThirdParty/DotNet/Linux/..."
-setup_dotnet = "Engine/Build/BatchFiles/Linux/SetupDotnet.sh"
-if dotnet in code and setup_dotnet not in code:
-    code = code.replace(dotnet, f"{dotnet}\n{setup_dotnet}")
-
-writeFile(filtersXml, code)

commit 2e86459c85e4db499ab543e09b963046c794e536

Fix ue4BuildArgs not defined Regression from 9d0d2aa6de53b2f9eefbb5cdb34f1dfad91097ab

src/ue4docker/build.py
@@ -527,7 +527,7 @@ def build():
                     mainTags,
                     commonArgs
                     + config.platformArgs
-                    + ue4BuildArgs
+                    + minimalArgs
                     + infrastructureFlags,
                 )
                 builtImages.append("ue4-full")